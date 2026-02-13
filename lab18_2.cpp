3368764b362d65a22e4836a9433c4a63144690d9
Traipop <traipopwichiansarn@gmail.com>
2026-02-13 19:07:31 +0700

    lab18_2 completed

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -5,8 +5,20 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect *r1,Rect *r2){
 
+	double overleft = max(r1->x, r2->x);
+	double overright = min(r1->x + r1->w, r2->x + r2->w);
+	double overtop = min(r1->y, r2->y);
+	double overbottom = max(r1->y - r1->h, r2->y - r2->h);
 
+	double width= overright - overleft;
+	double height = overtop - overbottom;
 
+	if(width > 0 && height > 0){
+		return width*height;
+	} else {
+		return 0;
+	}
 }
+
